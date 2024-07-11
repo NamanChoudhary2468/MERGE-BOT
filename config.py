40f4c051cabceab07daaab115f67fb1e15ec74fc
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2024-07-11 13:06:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ import os
 
 class Config(object):
     API_HASH = os.environ.get("1c2c6d7244d4576b2baab88337b1233a")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    BOT_TOKEN = os.environ.get("7098978512:AAHMV8j-r5YBSCryvWHIkMrE8vtNbbo7bxw")
     TELEGRAM_API = os.environ.get("25062134")
     OWNER = os.environ.get("6302921275")
     OWNER_USERNAME = os.environ.get("Spider_Man_02")
     PASSWORD = os.environ.get("PASSWORD")
     DATABASE_URL = os.environ.get("mongodb+srv://File2Link:File2Link@cluster0.ygj9iuu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
+    LOGCHANNEL = os.environ.get("-1002208942976")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
     IS_PREMIUM = False
